e161847f541efbe878bde03454c9a7f6d7aaf28e
Natalia Santos <natalia.santos@crmbonus.com>
2024-01-24 11:04:56 -0300

    correção do código

--- cypress/src/script.js
+++ cypress/src/script.js
@@ -17,7 +17,7 @@ document.querySelector('#phone-checkbox')
   })
 
 document.querySelector('button[type="submit"]')
-  .addEventListener('click', (event) {
+  .addEventListener('click', function(event) {
     event.preventDefault()
     const firstNameField = document.getElementById('firstName')
     const lastNameField = document.getElementById('lastName')
